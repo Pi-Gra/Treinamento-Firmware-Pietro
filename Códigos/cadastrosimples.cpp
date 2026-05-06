18115614ec8ca9bc8d216fcffb993c88cda0970d
Pi-Gra <pi@sigmag.com.br>
2026-05-06 14:17:16 -0300

    Código do Problema Cadastro Simples
    Código desenvolvido para satisfazer o problema do cadastro simples

---
+++ Códigos/cadastrosimples.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int idade;
+    float altura;
+
+    cout << "Idade: ";
+    cin >> idade;
+
+    cout << "Altura: ";
+    cin >> altura;
+
+    cout << "Voce tem " << idade << " anos e mede " << altura << " metros de altura" << endl;
+}
